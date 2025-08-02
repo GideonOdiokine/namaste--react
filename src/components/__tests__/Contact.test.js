fb996a80d18c04409d1ae43bf97ad33dc73214b4
gideonodiokine <gidysmartodiokineh@gmail.com>
2025-08-02 03:51:56 +0100

    test: refactor Contact component tests for clarity and structure

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -1,18 +1,33 @@
 import { render, screen } from "@testing-library/react";
 import Contact from "../Contact";
-import '@testing-library/jest-dom'
+import "@testing-library/jest-dom";
 
-test("Should load contact us component", () => {
-  render(<Contact />);
+describe("Contact Us Page Test Case", () => {
+  test("Should load contact us component", () => {
+    render(<Contact />);
 
-  const heading = screen.getByRole("heading");
-  expect(heading).toBeInTheDocument()
-});
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+  });
 
+  test("Should load button inside Contact component", () => {
+    render(<Contact />);
 
-test("Should load button inside Contact component", () => {
-  render(<Contact />);
+    const button = screen.getByText("Submit");
+    expect(button).toBeInTheDocument();
+  });
 
-  const button = screen.getByRole("button");
-  expect(button).toBeInTheDocument()
+  test("Should load name input inside Contact component", () => {
+    render(<Contact />);
+
+    const nameInput = screen.getByPlaceholderText("Name");
+    expect(nameInput).toBeInTheDocument();
+  });
+
+  test("Should load all 2 input boxes with name inside Contact component", () => {
+    render(<Contact />);
+
+    const inputBoxes = screen.getAllByRole("textbox");
+    expect(inputBoxes.length).toBe(3);
+  });
 });
