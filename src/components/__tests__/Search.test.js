5ccd319d8ffb6af770545934461628dc3d47f0e7
gideonodiokine <gidysmartodiokineh@gmail.com>
2025-08-05 12:47:32 +0100

    test: enhance Search component tests by adding fireEvent interactions and improving structure

--- src/components/__tests__/Search.test.js
+++ src/components/__tests__/Search.test.js
@@ -1,9 +1,9 @@
 import Bodyi from "../Bodyi";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import MOCK_DATA from "../mocks/mockResListData.json";
 import { act } from "react";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
 global.fetch = jest.fn(() => {
   return Promise.resolve({
@@ -21,4 +21,19 @@ it("should render the Body Component with Search", async () => {
       </BrowserRouter>
     );
   });
+
+
+
+  const searchBtn = screen.getByRole("button", { name: "Search" });
+
+  const searchInput = screen.getByTestId("searchInput");
+
+  fireEvent.change(searchInput, { target: { value: "mcDonald's" } });
+
+  fireEvent.click(searchBtn);
+
+  // Screen should load 1 res card
+  const card = screen.getByTestId("resCard");
+
+  expect(card).toBeInTheDocument();
 });
